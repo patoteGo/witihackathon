6723f99a090478bd3da73dcb72257430dce0a527
Patricio Gonzalez <patote.gonzalez@gmail.com>
2020-12-05 22:52:11 -0300

    footer again

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ module.exports = {
         title: ['"Press Start 2P"'],
       },
       backgroundImage: theme => ({
-        'hero-pattern': "url('/img/hero-pattern.png')",
-        'footer-texture': "url('/img/footer-texture.png')",
+        'hero-pattern': "url('/witihackathon/img/hero-pattern.png')",
+        'footer-texture': "url('/witihackathon/img/footer-texture.png')",
        })
     },
   },
